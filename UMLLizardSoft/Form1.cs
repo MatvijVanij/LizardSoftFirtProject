13228f5d68fa967d902c0a411761a20505cb97e6
Matvijivan <matvijivan>
2021-04-05 07:05:33 -0700

    corect

--- UMLLizardSoft/Form1.cs
+++ UMLLizardSoft/Form1.cs
@@ -17,6 +17,7 @@ namespace UMLLizardSoft
         Bitmap _tmpBitmap;
         Graphics _graphics;
         Pen _pen;
+        Pen _penEnd;
         Point _mouseStart;
 
         int x1, y1, x2, y2;
@@ -30,6 +31,7 @@ namespace UMLLizardSoft
         private void Form1_Load(object sender, EventArgs e)
         {
             _mainBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            _penEnd = new Pen(Color.Red, 3);
             _pen = new Pen(Color.Red, 3);
             _graphics = Graphics.FromImage(_mainBitmap);
             _graphics.Clear(Color.White);
@@ -38,13 +40,13 @@ namespace UMLLizardSoft
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-            if (e.Button == MouseButtons.Left)
+            _isButtonPressed = true;
+            if (_isButtonPressed)
             {
                 _mouseStart = e.Location;
                 x1 = e.X;
                 y1 = e.Y;
             }
-            _isButtonPressed = true;
         }
 
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
@@ -54,7 +56,7 @@ namespace UMLLizardSoft
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
-            if (e.Button == MouseButtons.Left && _isButtonPressed)
+            if (_isButtonPressed)
             {
                 x2 = e.X;
                 y2 = e.Y;
@@ -69,9 +71,9 @@ namespace UMLLizardSoft
 
                 double ugol = Math.Atan2(x1 - x2, y1 - y2);
 
-                _graphics.DrawLine(_pen,x2, y2, Convert.ToInt32(x2 + 25 * Math.Sin(0.3 + ugol)), Convert.ToInt32(y2 + 25 * Math.Cos(0.3 + ugol)));
-                _graphics.DrawLine(_pen, x2,y2, Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0.3)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0.3)));
-                _graphics.DrawLine(_pen, Convert.ToInt32(x2 + 25 * Math.Sin(0.3 + ugol)), Convert.ToInt32(y2 + 25 * Math.Cos(0.3 + ugol)), Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0.3)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0.3)));
+                _graphics.DrawLine(_penEnd,x2, y2, Convert.ToInt32(x2 + 25 * Math.Sin(0.3 + ugol)), Convert.ToInt32(y2 + 25 * Math.Cos(0.3 + ugol)));
+                _graphics.DrawLine(_penEnd, x2,y2, Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0.3)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0.3)));
+                _graphics.DrawLine(_penEnd, Convert.ToInt32(x2 + 25 * Math.Sin(0.3 + ugol)), Convert.ToInt32(y2 + 25 * Math.Cos(0.3 + ugol)), Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0.3)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0.3)));
                 
                 pictureBox1.Image = _tmpBitmap;
                 GC.Collect();
