94ffe1f6627837b7184fda2b7a4edbd820959a2a
Matvijivan <matvijivan>
2021-04-05 13:33:11 -0700

    add arrows

--- UMLLizardSoft/Form1.cs
+++ UMLLizardSoft/Form1.cs
@@ -18,10 +18,11 @@ namespace UMLLizardSoft
         Graphics _graphics;
         Pen _pen;
         Pen _penEnd;
-        Point _mouseStart;
+        Pen _penWhite;
 
         int x1, y1, x2, y2;
         bool _isButtonPressed = false;
+        private Brush redBrush;
 
         public Form1()
         {
@@ -31,8 +32,9 @@ namespace UMLLizardSoft
         private void Form1_Load(object sender, EventArgs e)
         {
             _mainBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-            _penEnd = new Pen(Color.Red, 3);
-            _pen = new Pen(Color.Red, 3);
+            _penEnd = new Pen(Color.Red, 4);
+            _pen = new Pen(Color.Red, 4);
+            _penWhite = new Pen(Color.White, 4);
             _graphics = Graphics.FromImage(_mainBitmap);
             _graphics.Clear(Color.White);
             pictureBox1.Image = _mainBitmap;
@@ -43,7 +45,6 @@ namespace UMLLizardSoft
             _isButtonPressed = true;
             if (_isButtonPressed)
             {
-                _mouseStart = e.Location;
                 x1 = e.X;
                 y1 = e.Y;
             }
@@ -61,25 +62,72 @@ namespace UMLLizardSoft
                 x2 = e.X;
                 y2 = e.Y;
 
-                _pen.DashStyle = DashStyle.Dash;
-
-
                 _tmpBitmap = (Bitmap)_mainBitmap.Clone();
                 _graphics = Graphics.FromImage(_tmpBitmap);
 
-                _graphics.DrawLine(_pen, x1, y1, x2, y2);
+                addArrowСomposition(_pen, x1, y1, x2, y2);
 
-                double ugol = Math.Atan2(x1 - x2, y1 - y2);
-
-                _graphics.DrawLine(_penEnd,x2, y2, Convert.ToInt32(x2 + 25 * Math.Sin(0.3 + ugol)), Convert.ToInt32(y2 + 25 * Math.Cos(0.3 + ugol)));
-                _graphics.DrawLine(_penEnd, x2,y2, Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0.3)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0.3)));
-                _graphics.DrawLine(_penEnd, Convert.ToInt32(x2 + 25 * Math.Sin(0.3 + ugol)), Convert.ToInt32(y2 + 25 * Math.Cos(0.3 + ugol)), Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0.3)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0.3)));
-                
                 pictureBox1.Image = _tmpBitmap;
                 GC.Collect();
-
             }
 
         }
+        public void addArrowImplementation(Pen _pen,int  x1,int  y1,int  x2,int  y2)// Имплиментацыя
+        {
+            double ugol = Math.Atan2(x1 - x2, y1 - y2);
+            _pen.DashStyle = DashStyle.Dash;
+            _graphics.DrawLine(_pen, x1, y1, Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0)));
+            _graphics.DrawLine(_penEnd, x2, y2, Convert.ToInt32(x2 + 25 * Math.Sin(0.4 + ugol)), Convert.ToInt32(y2 + 25 * Math.Cos(0.4 + ugol)));
+            _graphics.DrawLine(_penEnd, x2, y2, Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0.4)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0.4)));
+            _graphics.DrawLine(_penEnd, Convert.ToInt32(x2 + 25 * Math.Sin(0.4 + ugol)), Convert.ToInt32(y2 + 25 * Math.Cos(0.4 + ugol)), Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0.4)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0.4)));
+        }
+
+        public void addArrowAsotiation(Pen _pen, int x1, int y1, int x2, int y2)
+        {
+            double ugol = Math.Atan2(x1 - x2, y1 - y2);// Асоциация
+            _graphics.DrawLine(_pen, x1, y1, x2, y2);
+            _graphics.DrawLine(_penEnd, x2, y2, Convert.ToInt32(x2 + 25 * Math.Sin(0.4 + ugol)), Convert.ToInt32(y2 + 25 * Math.Cos(0.4 + ugol)));
+            _graphics.DrawLine(_penEnd, x2, y2, Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0.4)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0.4)));
+        }
+
+        public void addArrowInheritance(Pen _pen, int x1, int y1, int x2, int y2)//Наследование
+        {
+            double ugol = Math.Atan2(x1 - x2, y1 - y2);
+            _graphics.DrawLine(_pen, x1, y1, Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0)));
+            _graphics.DrawLine(_penEnd, x2, y2, Convert.ToInt32(x2 + 25 * Math.Sin(0.4 + ugol)), Convert.ToInt32(y2 + 25 * Math.Cos(0.4 + ugol)));
+            _graphics.DrawLine(_penEnd, x2, y2, Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0.4)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0.4)));
+            _graphics.DrawLine(_penEnd, Convert.ToInt32(x2 + 25 * Math.Sin(0.4 + ugol)), Convert.ToInt32(y2 + 25 * Math.Cos(0.4 + ugol)), Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0.4)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0.4)));
+        }
+
+        public void addArrowAggregation(Pen _pen, int x1, int y1, int x2, int y2)//Агрегация
+        {
+            double ugol = Math.Atan2(x1 - x2, y1 - y2);
+            _graphics.DrawLine(_pen, x1, y1, Convert.ToInt32(x2 + 50 * Math.Sin(ugol - 0)), Convert.ToInt32(y2 + 50 * Math.Cos(ugol - 0)));
+            _graphics.DrawLine(_pen, x2, y2, Convert.ToInt32(x2 + 25 * Math.Sin(0.4 + ugol)), Convert.ToInt32(y2 + 25 * Math.Cos(0.4 + ugol)));
+            _graphics.DrawLine(_pen, x2, y2, Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0.4)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0.4)));
+            _graphics.DrawLine(_pen, Convert.ToInt32(x2 + 50 * Math.Sin(ugol - 0)), Convert.ToInt32(y2 + 50 * Math.Cos(ugol - 0)), Convert.ToInt32(x2 + 25 * Math.Sin(0.4 + ugol)), Convert.ToInt32(y2 + 25 * Math.Cos(0.4 + ugol)));
+            _graphics.DrawLine(_pen, Convert.ToInt32(x2 + 50 * Math.Sin(ugol - 0)), Convert.ToInt32(y2 + 50 * Math.Cos(ugol - 0)), Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0.4)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0.4)));
+        }
+
+        public void addArrowСomposition(Pen _pen, int x1, int y1, int x2, int y2)//Композицыя
+        {
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+            double ugol = Math.Atan2(x1 - x2, y1 - y2);
+            
+            _graphics.DrawLine(_pen, x1, y1, Convert.ToInt32(x2 + 50 * Math.Sin(ugol - 0)), Convert.ToInt32(y2 + 50 * Math.Cos(ugol - 0)));
+            _graphics.DrawLine(_pen, x2, y2, Convert.ToInt32(x2 + 25 * Math.Sin(0.4 + ugol)), Convert.ToInt32(y2 + 25 * Math.Cos(0.4 + ugol)));
+            _graphics.DrawLine(_pen, x2, y2, Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0.4)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0.4)));
+            _graphics.DrawLine(_pen, Convert.ToInt32(x2 + 50 * Math.Sin(ugol - 0)), Convert.ToInt32(y2 + 50 * Math.Cos(ugol - 0)), Convert.ToInt32(x2 + 50 * Math.Sin(ugol - 0)), Convert.ToInt32(y2 + 50 * Math.Cos(ugol - 0)));
+            _graphics.DrawLine(_pen, Convert.ToInt32(x2 + 50 * Math.Sin(ugol - 0)), Convert.ToInt32(y2 + 50 * Math.Cos(ugol - 0)), Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0.4)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0.4)));
+            
+            Point point1 = new Point(Convert.ToInt32(x2 + 50 * Math.Sin(ugol - 0)), Convert.ToInt32(y2 + 50 * Math.Cos(ugol - 0)));
+            Point point2 = new Point(Convert.ToInt32(x2 + 25 * Math.Sin(0.4 + ugol)), Convert.ToInt32(y2 + 25 * Math.Cos(0.4 + ugol)));
+            Point point3 = new Point(x2, y2);
+            Point point4 = new Point(Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0.4)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0.4)));
+            Point[] points = { point1, point2, point3, point4 };
+
+            _graphics.FillClosedCurve(redBrush,points);
+        }
+
     }
 }
