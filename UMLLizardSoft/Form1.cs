535720fccd73a6e94f6f3163fa21a0b1ce7f33de
Matvijivan <matvijivan>
2021-04-05 06:51:14 -0700

    arrow drawing in mouseMove

--- UMLLizardSoft/Form1.cs
+++ UMLLizardSoft/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +14,13 @@ namespace UMLLizardSoft
     public partial class Form1 : Form
     {
         Bitmap _mainBitmap;
+        Bitmap _tmpBitmap;
         Graphics _graphics;
         Pen _pen;
-        Bitmap _tmpBitmap;
+        Point _mouseStart;
+
+        int x1, y1, x2, y2;
+        bool _isButtonPressed = false;
 
         public Form1()
         {
@@ -33,16 +38,45 @@ namespace UMLLizardSoft
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-
+            if (e.Button == MouseButtons.Left)
+            {
+                _mouseStart = e.Location;
+                x1 = e.X;
+                y1 = e.Y;
+            }
+            _isButtonPressed = true;
         }
 
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
-
+            _isButtonPressed = false;
         }
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
+            if (e.Button == MouseButtons.Left && _isButtonPressed)
+            {
+                x2 = e.X;
+                y2 = e.Y;
+
+                _pen.DashStyle = DashStyle.Dash;
+
+
+                _tmpBitmap = (Bitmap)_mainBitmap.Clone();
+                _graphics = Graphics.FromImage(_tmpBitmap);
+
+                _graphics.DrawLine(_pen, x1, y1, x2, y2);
+
+                double ugol = Math.Atan2(x1 - x2, y1 - y2);
+
+                _graphics.DrawLine(_pen,x2, y2, Convert.ToInt32(x2 + 25 * Math.Sin(0.3 + ugol)), Convert.ToInt32(y2 + 25 * Math.Cos(0.3 + ugol)));
+                _graphics.DrawLine(_pen, x2,y2, Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0.3)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0.3)));
+                _graphics.DrawLine(_pen, Convert.ToInt32(x2 + 25 * Math.Sin(0.3 + ugol)), Convert.ToInt32(y2 + 25 * Math.Cos(0.3 + ugol)), Convert.ToInt32(x2 + 25 * Math.Sin(ugol - 0.3)), Convert.ToInt32(y2 + 25 * Math.Cos(ugol - 0.3)));
+                
+                pictureBox1.Image = _tmpBitmap;
+                GC.Collect();
+
+            }
 
         }
     }
