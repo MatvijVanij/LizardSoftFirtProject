e921f530207a9aaa1b13d8b0aefdaf8eda41b740
Matvijivan <matvijivan>
2021-04-05 06:35:14 -0700

    addPicturebox and  mouse avents

--- UMLLizardSoft/Form1.cs
+++ UMLLizardSoft/Form1.cs
@@ -12,9 +12,38 @@ namespace UMLLizardSoft
 {
     public partial class Form1 : Form
     {
+        Bitmap _mainBitmap;
+        Graphics _graphics;
+        Pen _pen;
+        Bitmap _tmpBitmap;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _mainBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            _pen = new Pen(Color.Red, 3);
+            _graphics = Graphics.FromImage(_mainBitmap);
+            _graphics.Clear(Color.White);
+            pictureBox1.Image = _mainBitmap;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+
         }
     }
 }
