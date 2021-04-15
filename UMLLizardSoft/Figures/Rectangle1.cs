9c77a72ad79ed4d24cac4f9fd0fe2ba1b7607e39
Matvijivan <matvijivan>
2021-04-15 18:04:44 +0300

    add drawString

--- UMLLizardSoft/Figures/Rectangle1.cs
+++ UMLLizardSoft/Figures/Rectangle1.cs
@@ -4,15 +4,20 @@ using System.Drawing;
 
 namespace UMLLizardSoft.Figures
 {
-    public class Rectangle1 :AbstractRectangle
+    public class Rectangle1 : AbstractRectangle
     {
         public override void Draw(Graphics graphics, Pen pen)
         {
+            string str = "Claas Name";
+            Font myFont = new Font("Arial", 16);
+            SolidBrush myBrush = new SolidBrush(Color.Black);
+
             int height = EndPoint.Y - StartPoint.Y;
             int width = EndPoint.X - StartPoint.X;
 
+            graphics.DrawString(str, myFont, myBrush, StartPoint.X,StartPoint.Y);
             graphics.DrawRectangle(pen, StartPoint.X, StartPoint.Y, width, height);
-            //graphics.DrawRectangle(pen, StartPoint.X, StartPoint.Y, width, (int)(height * 0.18));
+            graphics.DrawLine(pen, StartPoint.X, StartPoint.Y + 30, EndPoint.X, StartPoint.Y + 30);
         }
     }
 }
